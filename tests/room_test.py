1897301c334cda668f08684ebf1f64e67bbeaf47
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-10 12:37:35 +0000

    wrote tests for room

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,6 +6,12 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
    
    def setUp(self):
-      self.room = Room(1)
-      self.song = Song("Jailhouse Rock", "Elvis", "Rock")
-      self.guest = Guest("Terry")
+       self.room = Room(1)
+       self.song = Song("Jailhouse Rock", "Elvis", "Rock")
+       self.guest = Guest("Terry")
+
+   def test_room_id(self):
+       self.assertEqual(1, self.room.id)
+
+   def test_song_artist(self):
+       self.assertEqual("Elvis", self.song.artist)
