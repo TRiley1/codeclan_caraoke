eeb42df875c8ec53ce37c1f06267aacd282e178e
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-10 12:17:35 +0000

    finalised tests with all actors

--- tests/tests.room_test.py
+++ tests/tests.room_test.py
@@ -1,7 +1,11 @@
 import unittest
 from src.room import Room
+from src.guest import Guest
+from src.song import Song 
 
 class TestRoom(unittest.TestCase):
    
    def setUp(self):
       self.room = Room(1)
+      self.song = Song("Jailhouse Rock", "Elvis", "Rock")
+      self.guest = Guest("Terry")
