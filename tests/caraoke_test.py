640e0148f12200288c664c700befb350812a8449
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-12 11:47:09 +0000

    small structure changes

--- tests/caraoke_test.py
+++ tests/caraoke_test.py
@@ -8,7 +8,7 @@ class TestCaraokeBar(unittest.TestCase):
         self.caraoke_bar = CaraokeBar("CCC", 100, 5)
         self.room = Room(1, 1)
         self.room2 = Room(2, 4)
-        self.room3 = Room(3,4)
+        self.room3 = Room(3, 4)
         self.guest = Guest("Tam", "Send me your location",10)
 
     def test_add_room_to_booth(self):
@@ -26,7 +26,7 @@ class TestCaraokeBar(unittest.TestCase):
         self.caraoke_bar.charge(self.caraoke_bar.entry_fee)
         self.assertEqual(105 , self.caraoke_bar.till)
 
-    def test_add_two_guests_to_room(self):
+    def test_overflow_into_room_2(self):
         self.guest2 = Guest("Del", "Lookers Street",12)
         self.caraoke_bar.add_room_to_booth(self.room)
         self.caraoke_bar.add_room_to_booth(self.room2)
